7f30809adaf866b0e7c74f8e851134a500252ca9
sudhir khanuja <sudhir.khanuja1@gmail.com>
2023-10-28 11:39:52 +0530

    about.js changed

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,9 +3,9 @@ import React, { Fragment } from 'react';
 const About = () => {
   return (
     <Fragment>
-      <h1>About This App</h1>
+      <h1>About This New App</h1>
       <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <p>Version: 2.0.0</p>
     </Fragment>
   );
 };
